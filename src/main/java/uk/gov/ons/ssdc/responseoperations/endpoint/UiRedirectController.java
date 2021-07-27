cbffec749e36d8c2fb2e3e344383a1a894ef179c
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-07-27 17:10:24 +0100

    Improve comment wording

--- src/main/java/uk/gov/ons/ssdc/responseoperations/endpoint/UiRedirectController.java
+++ src/main/java/uk/gov/ons/ssdc/responseoperations/endpoint/UiRedirectController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UiRedirectController {
-  // This redirects any request for something other than /api back to the root, so that React works
+  // This forwards any request for something other than /api back to the root, so that React works
   @GetMapping(value = "{_:^(?!index\\.html|api).*$}")
   public String forward() {
     return "forward:/";
