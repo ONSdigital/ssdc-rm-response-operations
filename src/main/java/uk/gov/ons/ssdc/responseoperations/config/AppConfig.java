f99fba201fe4510c8339f99c302b1443470ee6b0
LukeLoze1 <Luke.Loze@ext.ons.gov.uk>
2021-11-25 09:34:19 +0000

    fix local logging

--- src/main/java/uk/gov/ons/ssdc/responseoperations/config/AppConfig.java
+++ src/main/java/uk/gov/ons/ssdc/responseoperations/config/AppConfig.java
@@ -3,14 +3,21 @@ package uk.gov.ons.ssdc.responseoperations.config;
 import com.godaddy.logging.LoggingConfigs;
 import java.util.TimeZone;
 import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
+  @Value("$logging.profile")
+  private String loggingProfile;
 
   @PostConstruct
   public void init() {
-    LoggingConfigs.setCurrent(LoggingConfigs.getCurrent().useJson());
+    if( loggingProfile.equals("STRUCTURED")) {
+      LoggingConfigs.setCurrent(LoggingConfigs.getCurrent().useJson());
+    }
+    
     TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
   }
 }
