0856e384ab132848d9b4a39cf0b570607197406a
LukeLoze1 <Luke.Loze@ext.ons.gov.uk>
2021-12-13 09:44:24 +0000

    make a bog standard data class for Logging info as json

--- src/main/java/uk/gov/ons/ssdc/responseoperations/model/LoggingDto.java
+++ src/main/java/uk/gov/ons/ssdc/responseoperations/model/LoggingDto.java
@@ -1,3 +1,13 @@
 package uk.gov.ons.ssdc.responseoperations.model;
 
-public record LoggingDto(boolean audit, String userWhoCarriedOutAction, String actionType, String details) { }
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoggingDto {
+    private boolean audit;
+    private String userWhoCarriedOutAction;
+    private String actionType;
+    private String details;
+}
