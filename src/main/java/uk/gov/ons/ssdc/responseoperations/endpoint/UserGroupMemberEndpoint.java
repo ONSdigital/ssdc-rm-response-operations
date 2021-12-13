60aee2d6b30aa1f3c52f08df444be69e33271252
LukeLoze1 <Luke.Loze@ext.ons.gov.uk>
2021-12-13 13:52:49 +0000

    put User logging after success in removing user from group

--- src/main/java/uk/gov/ons/ssdc/responseoperations/endpoint/UserGroupMemberEndpoint.java
+++ src/main/java/uk/gov/ons/ssdc/responseoperations/endpoint/UserGroupMemberEndpoint.java
@@ -2,9 +2,11 @@ package uk.gov.ons.ssdc.responseoperations.endpoint;
 
 import com.godaddy.logging.Logger;
 import com.godaddy.logging.LoggerFactory;
+
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import uk.gov.ons.ssdc.common.model.entity.UserGroup;
 import uk.gov.ons.ssdc.common.model.entity.UserGroupMember;
-import uk.gov.ons.ssdc.responseoperations.model.LoggingDto;
+import uk.gov.ons.ssdc.responseoperations.model.RMLoggingDto;
 import uk.gov.ons.ssdc.responseoperations.model.dto.ui.UserGroupMemberDto;
 import uk.gov.ons.ssdc.responseoperations.model.repository.UserGroupMemberRepository;
 import uk.gov.ons.ssdc.responseoperations.model.repository.UserGroupRepository;
@@ -23,79 +25,77 @@ import uk.gov.ons.ssdc.responseoperations.model.repository.UserGroupRepository;
 @RestController
 @RequestMapping(value = "/api/userGroupMembers")
 public class UserGroupMemberEndpoint {
-  private static final Logger log = LoggerFactory.getLogger(SurveyEndpoint.class);
-  private final UserGroupMemberRepository userGroupMemberRepository;
-  private final UserGroupRepository userGroupRepository;
+    private static final Logger log = LoggerFactory.getLogger(SurveyEndpoint.class);
+    private final UserGroupMemberRepository userGroupMemberRepository;
+    private final UserGroupRepository userGroupRepository;
 
-  public UserGroupMemberEndpoint(
-      UserGroupMemberRepository userGroupMemberRepository,
-      UserGroupRepository userGroupRepository) {
-    this.userGroupMemberRepository = userGroupMemberRepository;
-    this.userGroupRepository = userGroupRepository;
-  }
-
-  @GetMapping("/findByGroup/{groupId}")
-  public List<UserGroupMemberDto> findByGroup(
-      @PathVariable(value = "groupId") UUID groupId,
-      @RequestAttribute("userEmail") String userEmail) {
-    UserGroup group =
-        userGroupRepository
-            .findById(groupId)
-            .orElseThrow(
-                () -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Group not found"));
-
-    if (group.getAdmins().stream()
-        .noneMatch(groupAdmin -> groupAdmin.getUser().getEmail().equals(userEmail))) {
-      throw new ResponseStatusException(HttpStatus.FORBIDDEN, "User not an admin");
+    public UserGroupMemberEndpoint(
+            UserGroupMemberRepository userGroupMemberRepository,
+            UserGroupRepository userGroupRepository) {
+        this.userGroupMemberRepository = userGroupMemberRepository;
+        this.userGroupRepository = userGroupRepository;
     }
 
-    return userGroupMemberRepository.findByGroup(group).stream()
-        .map(this::mapGroupMember)
-        .collect(Collectors.toList());
-  }
+    @GetMapping("/findByGroup/{groupId}")
+    public List<UserGroupMemberDto> findByGroup(
+            @PathVariable(value = "groupId") UUID groupId,
+            @RequestAttribute("userEmail") String userEmail) {
+        UserGroup group =
+                userGroupRepository
+                        .findById(groupId)
+                        .orElseThrow(
+                                () -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Group not found"));
 
-  @DeleteMapping("/{groupMemberId}")
-  public void removeUserFromGroup(
-      @PathVariable(value = "groupMemberId") UUID groupMemberId,
-      @RequestAttribute("userEmail") String userEmail) {
-    UserGroupMember userGroupMember =
-        userGroupMemberRepository
-            .findById(groupMemberId)
-            .orElseThrow(
-                () ->
-                    new ResponseStatusException(
-                        HttpStatus.BAD_REQUEST, "Group membership not found"));
+        if (group.getAdmins().stream()
+                .noneMatch(groupAdmin -> groupAdmin.getUser().getEmail().equals(userEmail))) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "User not an admin");
+        }
 
-    if (userGroupMember.getGroup().getAdmins().stream()
-        .noneMatch(groupAdmin -> groupAdmin.getUser().getEmail().equals(userEmail))) {
-      throw new ResponseStatusException(HttpStatus.FORBIDDEN, "User not an admin");
+        return userGroupMemberRepository.findByGroup(group).stream()
+                .map(this::mapGroupMember)
+                .collect(Collectors.toList());
     }
 
-    log.with(
-            new LoggingDto(
-                true,
-                userEmail,
-                "REMOVED USER FROM GROUP",
-                String.format(
-                    "User %s was removed from group %s",
-                    userGroupMember.getUser().getEmail(), userGroupMember.getGroup().getName())))
-        .info(
-            String.format(
-                "User %s was removed from group %s by %s",
-                userGroupMember.getUser().getEmail(),
-                userGroupMember.getGroup().getName(),
-                userEmail));
+    @DeleteMapping("/{groupMemberId}")
+    public void removeUserFromGroup(
+            @PathVariable(value = "groupMemberId") UUID groupMemberId,
+            @RequestAttribute("userEmail") String userEmail) {
+        UserGroupMember userGroupMember =
+                userGroupMemberRepository
+                        .findById(groupMemberId)
+                        .orElseThrow(
+                                () ->
+                                        new ResponseStatusException(
+                                                HttpStatus.BAD_REQUEST, "Group membership not found"));
 
-    userGroupMemberRepository.delete(userGroupMember);
-  }
+        if (userGroupMember.getGroup().getAdmins().stream()
+                .noneMatch(groupAdmin -> groupAdmin.getUser().getEmail().equals(userEmail))) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "User not an admin");
+        }
 
-  private UserGroupMemberDto mapGroupMember(UserGroupMember member) {
-    UserGroupMemberDto userGroupMemberDto = new UserGroupMemberDto();
-    userGroupMemberDto.setId(member.getId());
-    userGroupMemberDto.setGroupId(member.getGroup().getId());
-    userGroupMemberDto.setUserId(member.getUser().getId());
-    userGroupMemberDto.setUserEmail(member.getUser().getEmail());
-    userGroupMemberDto.setGroupName(member.getGroup().getName());
-    return userGroupMemberDto;
-  }
+        userGroupMemberRepository.delete(userGroupMember);
+
+        log.with(new RMLoggingDto(
+                        true,
+                        userEmail,
+                        "REMOVED USER FROM GROUP",
+                        String.format(
+                                "User %s was removed from group %s",
+                                userGroupMember.getUser().getEmail(), userGroupMember.getGroup().getName())))
+                .warn(String.format(
+                        "User %s was removed from group %s by %s",
+                        userGroupMember.getUser().getEmail(),
+                        userGroupMember.getGroup().getName(),
+                        userEmail));
+    }
+
+    private UserGroupMemberDto mapGroupMember(UserGroupMember member) {
+        UserGroupMemberDto userGroupMemberDto = new UserGroupMemberDto();
+        userGroupMemberDto.setId(member.getId());
+        userGroupMemberDto.setGroupId(member.getGroup().getId());
+        userGroupMemberDto.setUserId(member.getUser().getId());
+        userGroupMemberDto.setUserEmail(member.getUser().getEmail());
+        userGroupMemberDto.setGroupName(member.getGroup().getName());
+        return userGroupMemberDto;
+    }
 }
