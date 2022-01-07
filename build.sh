809ec7385fb41bc327f6abef59b35d5e944ea3ec
LukeLoze1 <44841627+LukeLoze1@users.noreply.github.com>
2022-01-07 14:38:48 +0000

    update Makefile (#69)
    * update Makefile
    
    * add ESLint to package.json
    
    * add eslint-jest to package.json
    
    * install Lint before using it
    
    * Make docker-build added

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@ mkdir -p src/main/resources/static
 rm -r src/main/resources/static/* || true
 rm -r ui/build/* || true
 cd ui
+npm install
 
 if ! npx npx eslint .; then
   echo "ESLint found issues"
   exit 1
 fi
 
-npm install
 npm run build
 cd ..
 cp -r ui/build/* src/main/resources/static
