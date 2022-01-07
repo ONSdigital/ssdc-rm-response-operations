4cc833289786741a4e426beb64262b9adb7a1533
liamtoozer <liam.toozer@gmail.com>
2022-01-07 13:25:22 +0000

    First pass

--- src/main/java/uk/gov/ons/ssdc/responseoperations/endpoint/AuthorisationEndpoint.java
+++ src/main/java/uk/gov/ons/ssdc/responseoperations/endpoint/AuthorisationEndpoint.java
@@ -2,12 +2,15 @@ package uk.gov.ons.ssdc.responseoperations.endpoint;
 
 import static uk.gov.ons.ssdc.common.model.entity.UserGroupAuthorisedActivityType.SUPER_USER;
 
+import com.godaddy.logging.Logger;
+import com.godaddy.logging.LoggerFactory;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
@@ -24,10 +27,23 @@ import uk.gov.ons.ssdc.responseoperations.model.repository.UserRepository;
 @RestController
 @RequestMapping(value = "/api/auth")
 public class AuthorisationEndpoint {
-  private final UserRepository userRepository;
+  private static final Logger log = LoggerFactory.getLogger(AuthorisationEndpoint.class);
 
-  public AuthorisationEndpoint(UserRepository userRepository) {
+  private final UserRepository userRepository;
+  private final boolean dummyUserIdentityAllowed;
+  private final String dummySuperUserIdentity;
+
+  public AuthorisationEndpoint(
+      UserRepository userRepository,
+      @Value("${dummyuseridentity-allowed}") boolean dummyUserIdentityAllowed,
+      @Value("${dummysuperuseridentity}") String dummySuperUserIdentity) {
     this.userRepository = userRepository;
+    this.dummyUserIdentityAllowed = dummyUserIdentityAllowed;
+    this.dummySuperUserIdentity = dummySuperUserIdentity;
+
+    if (dummyUserIdentityAllowed) {
+      log.error("*** SECURITY ALERT *** IF YOU SEE THIS IN PRODUCTION, SHUT DOWN IMMEDIATELY!!!");
+    }
   }
 
   @GetMapping
@@ -35,8 +51,9 @@ public class AuthorisationEndpoint {
       @RequestParam(required = false, value = "surveyId") Optional<UUID> surveyId,
       @RequestAttribute("userEmail") String userEmail) {
 
-    // TODO: Remove this before releasing to production!
-    if (userEmail.equals("dummy@fake-email.com")) {
+    if (dummyUserIdentityAllowed && userEmail.equals(dummySuperUserIdentity)) {
+      // Dummy test super user is fully authorised, bypassing all security
+      // This is **STRICTLY** for ease of dev/testing in non-production environments
       return Set.of(UserGroupAuthorisedActivityType.values());
     }
 
