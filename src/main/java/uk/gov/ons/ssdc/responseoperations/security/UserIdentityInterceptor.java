75c189c7e2894915d3ba31c279effd7e408f38ce
ryangrundy7 <35301276+ryangrundy7@users.noreply.github.com>
2024-02-26 11:34:35 +0000

    Upgrade to spring boot 3 (#164)

--- src/main/java/uk/gov/ons/ssdc/responseoperations/security/UserIdentityInterceptor.java
+++ src/main/java/uk/gov/ons/ssdc/responseoperations/security/UserIdentityInterceptor.java
@@ -1,7 +1,7 @@
 package uk.gov.ons.ssdc.responseoperations.security;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 public class UserIdentityInterceptor implements HandlerInterceptor {
